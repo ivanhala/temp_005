f5c977c41d0482a2b1bf1dc71ce0f1d0f3812200
Ivan <ivan@plugion.org>
2021-10-26 13:23:49 +0300

    Create example.js

---
+++ example.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function( $ ){
+	jQuery(document).on( 'wbk_timeslots_rendered', function(){
+     	jQuery('.wbk-slot-time').each(function(){
+             if( jQuery(this).html() == '10:00 - 15:00<br>' ){
+                 jQuery(this).html('Turno de mañana');
+             }
+             if( jQuery(this).html() == '16:00 - 20:00<br>' ){
+                 jQuery(this).html('Turno de tarde');
+             }
+			 if( jQuery(this).html() == '10:00 - 13:00<br>' ){
+                 jQuery(this).html('Turno de mañana');
+             }
+             if( jQuery(this).html() == '14:00 - 17:00<br>' ){
+                 jQuery(this).html('Turno mediodía');
+             }
+             if( jQuery(this).html() == '17:00 - 20:00<br>' ){
+                 jQuery(this).html('Turno mediodía');
+             }
+        });
+    }); 
+});
